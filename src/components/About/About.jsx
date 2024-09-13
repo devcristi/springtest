82f5aad08f7326cee8a7bd1160601d996c336109
cristi <cristiandanielboabes@gmail.com>
2024-09-13 20:46:09 +0300

    .

---
+++ src/components/About/About.jsx
@@ -0,0 +1,12 @@
+import react from 'react';
+import './About.css';
+
+const About = () => {
+    return (
+        <div id='about'>
+            <h1>About</h1>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import react from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div id='home'>
+            <h1>Home</h1>
+        </div>
+    );
+}    
+
+export default Home;
